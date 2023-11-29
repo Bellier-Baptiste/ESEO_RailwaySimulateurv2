a89eac62ab844561e040954303024173f016e538
Benoît Vavasseur <benoit.vavasseur@reseau.eseo.fr>
2023-11-29 18:24:07 +0100

    [7] [DEV] [0.0.1]
    - output files are archived with timestamps, existing files are not overwritten but their names are incremented, archived folder is automatically created if does not exist
    
    TODO :
    - more tests on archiving output files
    - archive JSON and XML files

--- network-journey-simulator/src/tools/csvCreator.go
+++ network-journey-simulator/src/tools/csvCreator.go
@@ -11,6 +11,7 @@ Author :
   - Team v1
   - Team v2
   - Paul TRÉMOUREUX (quality check)
+  - Benoît VAVASSEUR
 
 License : MIT License
 
@@ -42,10 +43,10 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 // var basePath = os.Getenv("GOPATH")
-var projectPath = ""
 
 /*
 CsvFile is a struct that represents a csv file.
@@ -64,10 +65,10 @@ type CsvFile struct {
 }
 
 /*
-check is a function that checks if an error is nil.
+check is a utility function that panics if the passed error is not nil.
 
 Param :
-  - e error : The error to check
+  - e error : The error to check.
 */
 func check(e error) {
 	if e != nil {
@@ -107,44 +108,33 @@ func NewFile(name string, columns []string) CsvFile {
 }
 
 /*
-createFile creates a new csv file with a given name, columns and delimiter.
+createFile creates a new csv file in the 'output' directory and archives it in
+a timestamped folder.
+It writes the headers to the file if configured in the global config.
 
 Param :
-  - name string : The name of the file
-  - columns []string : The columns of the file
-  - delimiter string : The delimiter of the file
+  - name string : The name of the file.
+  - columns []string : The columns of the file.
+  - delimiter string : The delimiter of the file.
 
 Return :
-  - CsvFile : The created file
+  - CsvFile : The created and archived CsvFile instance.
 */
 func createFile(name string, columns []string, delimiter string) CsvFile {
 	currentPath, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	check(err)
+
 	basePath := strings.Replace(currentPath, "src\\main", "", -1)
 	basePath = strings.Replace(basePath, "\\src", "", -1)
 	basePath = strings.Replace(basePath, "\\configs", "", -1)
 	basePath = strings.Replace(basePath, "\\models", "", -1)
 	basePath = strings.Replace(basePath, "\\simulator", "", -1)
 	basePath = strings.Replace(basePath, "\\tools", "", -1)
+
 	outputFolderPath := filepath.Join(basePath, "output/")
+	checkAndCreateDir(outputFolderPath)
 
-	outputExists, err := exists(outputFolderPath)
-
-	check(err)
-
-	if !outputExists {
-		err := os.Mkdir(outputFolderPath, 0777)
-		if err != nil {
-			return CsvFile{}
-		}
-	}
-
-	// Delete "src\main" if there (when launching simulator with command line)
-	filePath := filepath.Join(basePath, projectPath, "output/"+name+".csv")
-
+	filePath := filepath.Join(outputFolderPath, name+".csv")
 	file, err := os.Create(filePath)
 	check(err)
 
@@ -160,37 +150,118 @@ func createFile(name string, columns []string, delimiter string) CsvFile {
 	check(err)
 
 	if config.CsvHeaders() {
-
-		firstRow := strings.Join(columns, delimiter)
-
-		firstRow = firstRow + "\n"
-
-		d1 := []byte(firstRow)
-
-		_, err = file.Write(d1)
+		writeCsvHeaders(file, columns, delimiter)
 	}
 
-	mode := 0644
-	csvMode := os.FileMode(mode)
+	check(file.Chmod(0644))
+	check(file.Sync())
 
-	err = file.Chmod(csvMode)
-	if err != nil {
-		return CsvFile{}
-	}
+	csv := CsvFile{path: filePath, delimiter: delimiter}
 
-	check(err)
-
-	err = file.Sync()
-	if err != nil {
-		return CsvFile{}
-	}
-
-	csv := CsvFile{
-		path:      filePath,
-		delimiter: delimiter,
-	}
+	archiveFile(filePath, basePath)
 
 	return csv
+}
+
+/*
+checkAndCreateDir checks for the existence of a directory at the given path and
+creates it if it doesn't exist.
+
+Param :
+  - path string : The path of the directory to check and create.
+*/
+func checkAndCreateDir(path string) {
+	exists, err := exists(path)
+	check(err)
+	if !exists {
+		check(os.MkdirAll(path, 0777))
+	}
+}
+
+/*
+writeCsvHeaders writes the provided columns as headers to the given file using
+the specified delimiter.
+
+Param :
+  - file *os.File : The file to write headers to.
+  - columns []string : The headers to write.
+  - delimiter string : The delimiter used in the CSV file.
+*/
+func writeCsvHeaders(file *os.File, columns []string, delimiter string) {
+	header := strings.Join(columns, delimiter) + "\n"
+	_, err := file.Write([]byte(header))
+	check(err)
+}
+
+/*
+archiveFile copies the specified file to an archive directory with a timestamp.
+
+Param :
+  - filePath string : The path of the file to archive.
+  - basePath string : The base path of the project from which the archive
+    directory is derived.
+*/
+func archiveFile(filePath, basePath string) {
+	timestamp := time.Now().Format("2006-01-02_15-04")
+
+	archivesBasePath := filepath.Join(basePath, "..", "archives")
+
+	checkAndCreateDir(archivesBasePath)
+
+	archiveFolderPath := filepath.Join(archivesBasePath, timestamp)
+
+	exists, err := exists(archiveFolderPath)
+	check(err)
+	if exists {
+		archiveFolderPath = incrementFolderName(archiveFolderPath)
+	}
+	check(os.MkdirAll(archiveFolderPath, 0777))
+
+	archiveFilePath := filepath.Join(archiveFolderPath, filepath.Base(filePath))
+	check(copyFile(filePath, archiveFilePath))
+}
+
+/*
+incrementFolderName appends a numerical suffix to a folder name until
+a non-existent name is found.
+
+Param :
+  - path string : The original folder path.
+
+Return :
+  - string : The new folder path with a unique numerical suffix.
+*/
+func incrementFolderName(path string) string {
+	i := 2
+	newPath := fmt.Sprintf("%s (%d)", path, i)
+	for {
+		exists, err := exists(newPath)
+		check(err)
+		if !exists {
+			break
+		}
+		i++
+		newPath = fmt.Sprintf("%s (%d)", path, i)
+	}
+	return newPath
+}
+
+/*
+copyFile copies a file from the source path to the destination path.
+
+Param :
+  - src string : The source file path.
+  - dst string : The destination file path.
+
+Return :
+  - error : Error if any occurred during the file copy.
+*/
+func copyFile(src, dst string) error {
+	input, err := os.ReadFile(src)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(dst, input, 0644)
 }
 
 /*
@@ -278,14 +349,14 @@ func (csv *CsvFile) WriteMultiple(contents [][]string) {
 }
 
 /*
-exists checks if a file exists.
+exists checks if a file or directory exists at a given path.
 
 Param :
-  - path string : The path of the file
+  - path string : The path to check.
 
 Return :
-  - bool : True if the file exists, false otherwise
-  - error : The error if any
+  - bool : True if the file or directory exists, false otherwise.
+  - error : Error if any occurred during the check.
 */
 func exists(path string) (bool, error) {
 	_, err := os.Stat(path)
