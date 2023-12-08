9dcdb30f7c40dc06cf1a079f5f52f6a78b892352
bonamyal <alexis.bonamy@reseau.eseo.fr>
2023-12-08 15:04:32 +0100

    [41] [DEV] [0.0.3] Refactoring attendance peak to gaussian peak
    TODO : implement ramp peak in the simulator

--- network-journey-simulator/src/models/eventGaussianPeak.go
+++ network-journey-simulator/src/models/eventGaussianPeak.go
@@ -101,160 +101,160 @@ func NewEventGaussianPeak(start time.Time, end time.Time, peak time.Time,
 GetStart returns the start attribute of the EventGaussianPeak struct.
 
 Param :
-  - eap *EventGaussianPeak : the EventGaussianPeak struct
+  - egp *EventGaussianPeak : the EventGaussianPeak struct
 
 Return :
   - time.Time : the start attribute of the EventGaussianPeak struct
 */
-func (eap *EventGaussianPeak) GetStart() time.Time {
-	return eap.start
+func (egp *EventGaussianPeak) GetStart() time.Time {
+	return egp.start
 }
 
 /*
 SetStart sets the start attribute of the EventGaussianPeak struct.
 
 Param :
-  - eap *EventGaussianPeak : the EventGaussianPeak struct
+  - egp *EventGaussianPeak : the EventGaussianPeak struct
   - start time.Time : the new value of the start attribute
 */
-func (eap *EventGaussianPeak) SetStart(start time.Time) {
-	eap.start = start
+func (egp *EventGaussianPeak) SetStart(start time.Time) {
+	egp.start = start
 }
 
 /*
 GetEnd returns the end attribute of the EventGaussianPeak struct.
 */
-func (eap *EventGaussianPeak) GetEnd() time.Time {
-	return eap.end
+func (egp *EventGaussianPeak) GetEnd() time.Time {
+	return egp.end
 }
 
 /*
 SetEnd sets the end attribute of the EventGaussianPeak struct.
 
 Param :
-  - eap *EventGaussianPeak : the EventGaussianPeak struct
+  - egp *EventGaussianPeak : the EventGaussianPeak struct
   - end time.Time : the new value of the end attribute
 */
-func (eap *EventGaussianPeak) SetEnd(end time.Time) {
-	eap.end = end
+func (egp *EventGaussianPeak) SetEnd(end time.Time) {
+	egp.end = end
 }
 
 /*
 GetPeak returns the peak attribute of the EventGaussianPeak struct.
 
 Param :
-  - eap *EventGaussianPeak : the EventGaussianPeak struct
+  - egp *EventGaussianPeak : the EventGaussianPeak struct
 
 Return :
   - time.Time : the peak attribute of the EventGaussianPeak struct
 */
-func (eap *EventGaussianPeak) GetPeak() time.Time {
-	return eap.peak
+func (egp *EventGaussianPeak) GetPeak() time.Time {
+	return egp.peak
 }
 
 /*
 SetPeak sets the peak attribute of the EventGaussianPeak struct.
 
 Param :
-  - eap *EventGaussianPeak : the EventGaussianPeak struct
+  - egp *EventGaussianPeak : the EventGaussianPeak struct
   - peak time.Time : the new value of the peak attribute
 */
-func (eap *EventGaussianPeak) SetPeak(peak time.Time) {
-	eap.peak = peak
+func (egp *EventGaussianPeak) SetPeak(peak time.Time) {
+	egp.peak = peak
 }
 
 /*
 GetIdStation returns the idStation attribute of the EventGaussianPeak struct.
 
 Param :
-  - eap *EventGaussianPeak : the EventGaussianPeak struct
+  - egp *EventGaussianPeak : the EventGaussianPeak struct
 
 Return :
   - int : the idStation attribute of the EventGaussianPeak struct
 */
-func (eap *EventGaussianPeak) GetIdStation() int {
-	return eap.idStation
+func (egp *EventGaussianPeak) GetIdStation() int {
+	return egp.idStation
 }
 
 /*
 SetIdStation sets the idStation attribute of the EventGaussianPeak struct.
 
 Param :
-  - eap *EventGaussianPeak : the EventGaussianPeak struct
+  - egp *EventGaussianPeak : the EventGaussianPeak struct
   - id int : the new value of the idStation attribute
 */
-func (eap *EventGaussianPeak) SetIdStation(id int) {
-	eap.idStation = id
+func (egp *EventGaussianPeak) SetIdStation(id int) {
+	egp.idStation = id
 }
 
 /*
 GetPeakSize returns the size attribute of the EventGaussianPeak struct.
 
 Param :
-  - eap *EventGaussianPeak : the EventGaussianPeak struct
+  - egp *EventGaussianPeak : the EventGaussianPeak struct
 
 Return :
   - int : the peakSize attribute of the EventGaussianPeak struct
 */
-func (eap *EventGaussianPeak) GetPeakSize() int {
-	return eap.peakSize
+func (egp *EventGaussianPeak) GetPeakSize() int {
+	return egp.peakSize
 }
 
 /*
 SetPeakSize sets the size attribute of the EventGaussianPeak struct.
 
 Param :
-  - eap *EventGaussianPeak : the EventGaussianPeak struct
+  - egp *EventGaussianPeak : the EventGaussianPeak struct
   - s int : the new value of the size attribute
 */
-func (eap *EventGaussianPeak) SetPeakSize(s int) {
-	eap.peakSize = s
+func (egp *EventGaussianPeak) SetPeakSize(s int) {
+	egp.peakSize = s
 }
 
 /*
 GetPeakWidth returns the width attribute of the EventGaussianPeak struct.
 
 Param :
-  - eap *EventGaussianPeak : the EventGaussianPeak struct
+  - egp *EventGaussianPeak : the EventGaussianPeak struct
 
 Return :
   - int : the peakWidth attribute of the EventGaussianPeak struct
 */
-func (eap *EventGaussianPeak) GetPeakWidth() int {
-	return eap.peakWidth
+func (egp *EventGaussianPeak) GetPeakWidth() int {
+	return egp.peakWidth
 }
 
 /*
 SetPeakWidth sets the width attribute of the EventGaussianPeak struct.
 
 Param :
-  - eap *EventGaussianPeak : the EventGaussianPeak struct
+  - egp *EventGaussianPeak : the EventGaussianPeak struct
   - w int : the new value of the width attribute
 */
-func (eap *EventGaussianPeak) SetPeakWidth(w int) {
-	eap.peakWidth = w
+func (egp *EventGaussianPeak) SetPeakWidth(w int) {
+	egp.peakWidth = w
 }
 
 /*
 IsFinished returns the finished attribute of the EventGaussianPeak struct.
 
 Param :
-  - eap *EventGaussianPeak : the EventGaussianPeak struct
+  - egp *EventGaussianPeak : the EventGaussianPeak struct
 
 Return :
   - bool : the finished attribute of the EventGaussianPeak struct
 */
-func (eap *EventGaussianPeak) IsFinished() bool {
-	return eap.finished
+func (egp *EventGaussianPeak) IsFinished() bool {
+	return egp.finished
 }
 
 /*
 SetFinished sets the finished attribute of the EventGaussianPeak struct.
 
 Param :
-  - eap *EventGaussianPeak : the EventGaussianPeak struct
+  - egp *EventGaussianPeak : the EventGaussianPeak struct
   - f bool : the new value of the finished attribute
 */
-func (eap *EventGaussianPeak) SetFinished(f bool) {
-	eap.finished = f
+func (egp *EventGaussianPeak) SetFinished(f bool) {
+	egp.finished = f
 }
